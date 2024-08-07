775968a76890f1d5fa2af2bbb9e170f7f37444bd
FelixAG0302 <felixalejandroag0302@gmail.com>
2024-08-07 18:06:34 -0400

    Resultado final

--- TalentCorp/Controllers/CandidatosController.cs
+++ TalentCorp/Controllers/CandidatosController.cs
@@ -6,6 +6,9 @@ using TalentCorp.Entities;
 using System.Linq;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using OfficeOpenXml;
+using System.Data;
+
 
 namespace TalentCorp.Controllers
 {
@@ -68,6 +71,75 @@ namespace TalentCorp.Controllers
         }
 
 
+        public async Task<IActionResult> Export()
+        {
+            var candidatos = await context.Candidatos
+                .ToListAsync();
+
+            var table = ConvertToDataTable(candidatos);
+            ExcelPackage.LicenseContext = LicenseContext.Commercial;
+            using (var excelPackage = new ExcelPackage())
+            {
+                var worksheet = excelPackage.Workbook.Worksheets.Add("Sheet1");
+
+                for (var i = 0; i < table.Columns.Count; i++)
+                {
+                    worksheet.Cells[1, i + 1].Value = table.Columns[i].ColumnName;
+                }
+
+                for (var i = 0; i < table.Rows.Count; i++)
+                {
+                    for (var j = 0; j < table.Columns.Count; j++)
+                    {
+                        worksheet.Cells[i + 2, j + 1].Value = table.Rows[i][j].ToString();
+                    }
+                }
+
+                FileInfo? file = null;
+                try
+                {
+                    file = new FileInfo("candidatos.xlsx");
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Ha ocurrido un error al intentar exportar la información de los Candidatos.");
+                }
+
+                if (file != null)
+                {
+                    await excelPackage.SaveAsAsync(file);
+                }
+            }
+
+            Console.WriteLine("Datos exportados a Excel exitosamente.");
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        private static DataTable ConvertToDataTable<T>(IList<T> data)
+        {
+            var properties = typeof(T).GetProperties();
+            var dataTable = new DataTable();
+
+            foreach (var prop in properties)
+            {
+                dataTable.Columns.Add(prop.Name, Nullable.GetUnderlyingType(prop.PropertyType) ?? prop.PropertyType);
+            }
+
+            foreach (var item in data)
+            {
+                var row = dataTable.NewRow();
+                foreach (var prop in properties)
+                {
+                    row[prop.Name] = prop.GetValue(item) ?? DBNull.Value;
+                }
+
+                dataTable.Rows.Add(row);
+            }
+
+            return dataTable;
+        }
+
         // GET: Candidatos/Details/5
         public async Task<IActionResult> Details(int? id)
         {
@@ -113,6 +185,8 @@ namespace TalentCorp.Controllers
             );
 
             await context.Empleados.AddAsync(empleado);
+           
+            
             context.Candidatos.Remove(candidato);
 
             await context.SaveChangesAsync();
